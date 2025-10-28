bd3608343a4e5f44da615c1319b5cff60263dd9c
mohaaz12 <mohaaz3004@gmail.com>
2025-10-28 14:49:54 +0300

    notification

--- Mind_app/app.py
+++ Mind_app/app.py
@@ -1,61 +1,52 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail, Message
+from model import db
 from dotenv import load_dotenv
 import os
-
-
-from model import db, User, Client, Admin, Therapist, Notification, Centre, Session, Progress
-
-
-from routes.users import user_bp
-from routes.clients import client_bp
-from routes.therapists import therapist_bp
-from routes.admin import admin_bp
-from routes.center import centres_bp
-from routes.session import sessions_bp
-from routes.progess import progress_bp
-from routes.notification import notifications_bp
-from auth import auth_bp
-
-
-
 load_dotenv()
 
 
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///mindcare.db")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "your-secret-key")
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mindcare.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 
 db.init_app(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
-api = Api(app)
 CORS(app)
+api = Api(app)
 jwt = JWTManager(app)
+mail = Mail(app)
 
 
-app.register_blueprint(auth_bp)           
-app.register_blueprint(user_bp)          
-app.register_blueprint(client_bp)
-app.register_blueprint(therapist_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(notifications_bp)
-app.register_blueprint(progress_bp)
-app.register_blueprint(sessions_bp)
-app.register_blueprint(centres_bp)
+@app.route('/send')
+def send():
+    message = Message(
+        subject="Hello from MindCare App",
+        recipients=["test.mailtrap1234@gmail.com"],
+        sender=('Peter from MindCare', 'peter@mailtrap.club')
+    )
+    message.body = "This is a test email sent from the MindCare Flask application using Mailtrap."
+    mail.send(message)
+    return "Email sent successfully!"
 
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
