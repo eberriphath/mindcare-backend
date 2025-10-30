f3fb0f004d301ee9fd124707c407c6d658a9fdc2
mohaaz12 <mohaaz3004@gmail.com>
2025-10-30 10:54:46 +0300

    email notification

--- Mind_app/app.py
+++ Mind_app/app.py
@@ -5,34 +5,30 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
 from model import db, User, Client, Admin, Therapist, Notification, Centre, Session, Progress
 
-# Import Blueprints
 from routes.users import user_bp
 from routes.clients import client_bp
 from routes.therapists import therapist_bp
 from routes.admin import admin_bp
-from routes.center import centres_bp 
-from routes.session import sessions_bp 
+from routes.center import centres_bp
+from routes.session import sessions_bp
 from routes.progess import progress_bp
 from routes.notification import notifications_bp
+from auth import auth_bp
+from routes.send_mail import send_bp  
 
-
+load_dotenv()
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "postgresql+psycopg2://postgres.sggwzzmuagyrpxhdljid:JFU5ZfOEbimX5VbP@"
-    "aws-1-eu-north-1.pooler.supabase.com:5432/postgres"
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-app.config["JWT_SECRET_KEY"] = "your-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///mindcare.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "your-secret-key")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
-
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -41,7 +37,7 @@ api = Api(app)
 CORS(app)
 jwt = JWTManager(app)
 
-
+app.register_blueprint(auth_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(client_bp)
 app.register_blueprint(therapist_bp)
@@ -50,9 +46,9 @@ app.register_blueprint(notifications_bp)
 app.register_blueprint(progress_bp)
 app.register_blueprint(sessions_bp)
 app.register_blueprint(centres_bp)
+app.register_blueprint(send_bp)  
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
